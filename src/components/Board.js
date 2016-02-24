16ddee8c296c2b354ac57d1d09b49917d8808417
razvan393 <razvanghinea05@gmail.com>
2016-02-24 15:42:33 +0200

    Brute game state

--- src/components/Board.js
+++ src/components/Board.js
@@ -14,27 +14,82 @@ class Board extends React.Component {
       red: 9,
       black: 9,
       phase: 1,
+      isRemove: 0,
+      isSelected: 0,
+      redWin: 0,
+      blackWin: 0,
       array: [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
     };
   }
 
   onPointClick(player, index) {
-    if (this.state.array[index] === -1) {
-      if ((player == 1) && (this.state.red > 0)) {
-        this.state.player = 0;
-        this.state.array[index] = 1;
-        this.state.red -= 1;
-      } else if (this.state.black > 0) {
-        this.state.player = 1;
-        this.state.array[index] = 0;
-        this.state.black -= 1;
+    var obj = this.getNeighbours(index);
+    if (this.state.isRemove === 1) {
+      if(this.state.array[index] === 1) {
+        this.state.blackWin ++;
+      } else {
+        this.state.redWin ++;
       }
-      if ((this.state.red === 0) && (this.state.black === 0)) {
+      this.state.array[index] = -1;
+      this.state.isRemove = 0;
+      this.forceUpdate();
+    } else if (this.state.phase === 1) {
+      if (this.state.array[index] === -1) {
+        if ((player == 1) && (this.state.red > 0)) {
+          this.state.player = 0;
+          this.state.array[index] = 1;
+          this.state.red -= 1;
+        } else if (this.state.black > 0) {
+          this.state.player = 1;
+          this.state.array[index] = 0;
+          this.state.black -= 1;
+        }
+        this.forceUpdate();
+      }
+      if ((this.state.red === 0) && (this.state.black === 0) && (this.isMill(obj.line, index) || this.isMill(obj.column, index))) {
+        console.log((this.state.array[index] === 1 ? 'Red' : 'Black') + ' can move an opponents piece!');
+        this.state.isRemove = 1;
+      } else if ((this.state.red === 0) && (this.state.black === 0)) {
         this.state.phase = 2;
       }
+
+    } else if ((this.state.phase === 2) && (this.state.isSelected === 0)) {
+      if (this.state.array[index] !== -1) {
+        this.state.isSelected = index;
+        this.forceUpdate();
+      }
+    } else if ((this.state.phase === 2) && (this.state.isSelected !== 0)) {
+      if ((this.state.array[index] === -1) && (this.isInArray(this.getPosMove(this.state.isSelected), index))) {
+        this.state.array[index] = this.state.array[this.state.isSelected];
+        this.state.array[this.state.isSelected] = -1;
+        this.state.isSelected = 0;
+        this.forceUpdate();
+      }
+    }
+    if (this.isMill(obj.line, index) || this.isMill(obj.column, index)) {
+      console.log((this.state.array[index] === 1 ? 'Red' : 'Black') + ' can move an opponents piece!');
+      this.state.isRemove = 1;
       this.forceUpdate();
     }
+    if (this.state.redWin === 7){
+      this.resetState();
+      alert('Red won');
+    }
+    if (this.state.blackWin === 7){
+      this.resetState();
+      alert('Black won');
+    }
   }
+
+  isInArray(array, number) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === number) {
+        return true;
+      }
+    }
+    return false;
+  }
+
 
   getPosOnSquares(index) {
     return Math.floor(index / 8);
@@ -69,16 +124,16 @@ class Board extends React.Component {
     this.forceUpdate();
   }
 
-  getNeighbours(index) {
+  getPosMove(index) {
     var obj = [];
     var rectangle = Math.floor(index / 8);
-      obj.push(((index + 7) % 8)+(8 * rectangle));
-      obj.push(((index + 1) % 8)+(8 * rectangle));
+    obj.push(((index + 7) % 8) + (8 * rectangle));
+    obj.push(((index + 1) % 8) + (8 * rectangle));
     if (index % 2 === 1) {
       if (rectangle === 0) {
         obj.push(index + 8);
       } else if (rectangle === 2) {
-        obj.push(index-8);
+        obj.push(index - 8);
       } else {
         obj.push(index - 8);
         obj.push(index + 8);
@@ -87,17 +142,50 @@ class Board extends React.Component {
     return obj;
   }
 
-  isWin(array, index) {
-    var numberOf0 = 0;
-    var numberOf1 = 0;
-    for(let i=0; i<array.length; i++) {
-      if(this.state.array[array[i]] === 0) {
-        numberOf0 ++;
-      } else if(this.state.array[array[i]] === 1) {
-        numberOf1 ++;
+  getNeighbours(index) {
+    var line = [];
+    var column = [];
+    var rectangle = Math.floor(index / 8);
+    if (index % 2 === 1) {
+      line.push(((index + 7) % 8) + (8 * rectangle));
+      line.push(((index + 1) % 8) + (8 * rectangle));
+      if (rectangle === 0) {
+        column.push(index + 8);
+        column.push(index + 16);
+      } else if (rectangle === 2) {
+        column.push(index - 8);
+        column.push(index - 16);
+      } else {
+        column.push(index - 8);
+        column.push(index + 8);
+      }
+    } else {
+      if (index % 4 === 0) {
+        line.push(((index + 1) % 8) + (8 * rectangle));
+        line.push(((index + 2) % 8) + (8 * rectangle));
+        column.push(((index + 6) % 8) + (8 * rectangle));
+        column.push(((index + 7) % 8) + (8 * rectangle));
+      } else {
+        column.push(((index + 1) % 8) + (8 * rectangle));
+        column.push(((index + 2) % 8) + (8 * rectangle));
+        line.push(((index + 6) % 8) + (8 * rectangle));
+        line.push(((index + 7) % 8) + (8 * rectangle));
       }
     }
-    if(((numberOf0 >= 2)&&(this.state.array[index] === 0)) ||((numberOf1 >= 2)&&(this.state.array[index] === 1))){
+    return {line, column};
+  }
+
+  isMill(array, index) {
+    var numberOf0 = 0;
+    var numberOf1 = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (this.state.array[array[i]] === 0) {
+        numberOf0++;
+      } else if (this.state.array[array[i]] === 1) {
+        numberOf1++;
+      }
+    }
+    if (((numberOf0 >= 2) && (this.state.array[index] === 0)) || ((numberOf1 >= 2) && (this.state.array[index] === 1))) {
       return true;
     } else {
       return false;
@@ -107,12 +195,6 @@ class Board extends React.Component {
   render() {
     const points = [];
     const pointsOnGrid = [];
-    if (this.state.phase === 2)
-      for (let i = 0; i <= 23; i++) {
-        if(this.isWin(this.getNeighbours(i),i)){
-          console.log((this.state.array[i]===1 ? 'Red' : 'Black')+ ' Won' );
-        }
-      }
     for (let i = 0; i < 24; i++) {
       let left = '';
       let top = '';
